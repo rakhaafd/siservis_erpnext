f294c50198816aa94ef5aaa15c2256c255caf482
rakhaafd <rakhafausta07@gmail.com>
2026-06-08 16:59:58 +0700

    feat: update tickets custom whitelist

--- siservis/api/ticket.py
+++ siservis/api/ticket.py
@@ -30,3 +30,48 @@ def get_my_tickets():
         ],
         order_by="creation desc",
     )
+
+
+@frappe.whitelist()
+def get_my_invoices():
+    if frappe.session.user == "Guest":
+        frappe.throw("Login required")
+
+    customer = frappe.db.get_value(
+        "Customer", {"email_id": frappe.session.user}, "name"
+    )
+
+    if not customer:
+        return []
+
+    return frappe.get_all(
+        "Sales Invoice",
+        filters={"customer": customer},
+        fields=[
+            "name",
+            "posting_date",
+            "status",
+            "grand_total",
+            "outstanding_amount",
+            "currency",
+            "docstatus",
+        ],
+        order_by="creation desc",
+    )
+
+
+@frappe.whitelist()
+def get_invoice_detail(invoice_name):
+    if frappe.session.user == "Guest":
+        frappe.throw("Login required")
+
+    customer = frappe.db.get_value(
+        "Customer", {"email_id": frappe.session.user}, "name"
+    )
+
+    invoice = frappe.get_doc("Sales Invoice", invoice_name)
+
+    if invoice.customer != customer:
+        frappe.throw("Not permitted")
+
+    return invoice.as_dict()
